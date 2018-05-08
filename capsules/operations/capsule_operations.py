56804b1bb2f161c275f37611d2343196085fcd82
Krishna Seegoolam <kgs13@ying.doc.ic.ac.uk>
2018-05-08 12:28:17 +0100

    debug and move towards eager_execution

--- capsules/operations/capsule_operations.py
+++ capsules/operations/capsule_operations.py
@@ -8,18 +8,16 @@ import variables
 def fc_capsule_layer(input_tensor,scope_name, apply_weights=True,    share_weights_within_channel=False, output_vec_dimension=8,    apply_bias=False):
     '''Fully connected Capsule Layer '''
     with tf.name_scope(scope_name):
-        wth tf.name_scope('transform')
+        with tf.name_scope('transform'):
             input_tensor_shape = tf.shape(input_tensor)
             # [batch, vec, num_channels, height, width]
-            if apply_weights===True:
+            if(apply_weights==True):
                 with tf.variable_scope(scope_name):
                     if share_weights_within_channel==False:
-                        weights_shape_init= input_tensor_shape[0:2] +
-                            [output_vec_dimension]+input_tensor_shape[2::]
+                        weights_shape_init= input_tensor_shape[0:2] +       [output_vec_dimension]+input_tensor_shape[2::]
                         weights = variables.weight_variable(shape=weights_shape_init)
                     else:
-                        weights_shape_init = input_tensor_shape[0:2] +
-                            [output_vec_dimension] + input_tensor_shape[2]
+                        weights_shape_init = input_tensor_shape[0:2] +                            [output_vec_dimension] + input_tensor_shape[2]
                         weights = variables.weight_variable(shape=weights_shape_init)
                         weights = tf.tile(weights, [1,1,1, *input_tensor_shape[3::]])
                     # weights shape: [batch, vec, output_vec_dimension, num_channels, height, width]
@@ -40,7 +38,7 @@ def fc_capsule_layer(input_tensor,scope_name, apply_weights=True,    share_weigh
                 input_tensor_shape[2] = output_vec_dimension
             # [] [unfinished] - havent add a bias term to Wx, - it should be Wx+b!
             #  NB/ in the original Sabour et al code, the '+b' they refer to in 'Wx_plus_b' is where the bias term, b, corresponds to the bias terms that are passed to the update_routing method
-            if apply_bias === True:
+            if apply_bias == True:
                 raise NotImplemented
     return input_tensor, input_tensor_shape
 
@@ -50,28 +48,45 @@ def init_conv_2d(input_images, num_f_maps, scope_name, kernel_size=5):
         #input images = [batch, height, width]
         input_images = tf.expand_dims(input_images, 3)
         #input images = [batch, height, widht,1]
-        kernel = [kernel_size, kernel_size, 1, num_f_maps]
-        output = tf.nn.conv2d(input_images, [1,1,1,1], "SAME", name="Init2DConv")
+        kernel_shape = [kernel_size, kernel_size, 1, num_f_maps]
+        print(">>>>>>> Create kernel weights")
+        with tf.variable_scope(scope_name):
+            kernel = variables.weight_variable(kernel_shape)
+        print(">>>>>>> Convolve")
+        output = tf.nn.conv2d(input_images, kernel, [1,1,1,1], "SAME", name="Init2DConv")
         # [batch, height, width, num_f_maps]
-        output = tf.tranpose(output, [0,3,1,2])
+        output = tf.transpose(output, [0,3,1,2])
         # [batch, num_f_maps, height, width]
         # Now, expand_dims .t. each feature map is an atom i.e.
         # a 256Dim vector
         output = tf.expand_dims(output, 2)
         # now: [batch, vec_dim=num_f_maps, num_chanels=1, height, width]
-        output_tensor_shape = tf.shape(output)
+        output_tensor_shape = output.get_shape().as_list()
+        print(">>>>>>> Create some bias terms for Conv")
         with tf.variable_scope(scope_name):
+            '''print(">>>>>>> Slice")
+            bias_shape_tmp = tf.slice(output_tensor_shape, [1], [2])
+            bias_shape_tmp = tf.reshape(bias_shape_tmp, [-1])
+            print(">>>>>>> Concat")
+            bias_shape = tf.concat([[1], bias_shape_tmp, [1], [1]], axis=0)
+            # bias shape = [1,vec_dim,num_channel,1,1]'''
             bias_shape = output_tensor_shape[:]
             bias_shape[0] = 1
             bias_shape[-1] = 1
             bias_shape[-2] = 1
-            # bias shape = [1,vec_dim,num_channel,1,1]
+            print(">>>>>>> Bias creation")
+            print(output.get_shape().as_list()) # will output: 200, 256, 1, 28, 28 ater exe
             biases = variables.bias_variable(bias_shape)
-        retiled_bias_shape = output_tensor_shape[:]
+        retiled_bias_shape = output_tensor_shape[:] # [batch, vec, num_ch, h, w]
+        print(">>>>>>> Create retiling tensor (rank 1)")
+        ###shape_batch = tf.reshape(tf.slice(retiled_bias_shape, [0], [1]), [-1])
+        ###shape_h_w = tf.reshape(tf.slice(retiled_bias_shape, [3], [2]), [-1])
+        ###retiled_bias_shape = tf.concat([shape_batch, [1],[1], shape_h_w], axis=0)
         retiled_bias_shape[1] = 1
-        retiled_bias_shape[2] = 1
+        retiled_bias_shape[2] = 1 # [batch, 1, 1, h, w]
         biases = tf.tile(biases, retiled_bias_shape)
-        output = output + biases
+        print(">>>>>>> Add bias and conv output")
+        output = tf.add(output, biases)
         output = tf.nn.relu(output)
     return output
 
@@ -80,7 +95,7 @@ def init_conv_2d(input_images, num_f_maps, scope_name, kernel_size=5):
 def convolutional_capsule_layer(input_tensor, kernel_height, kernel_width, scope_name,output_kernel_vec_dim=8, strides=[1, 1],
 convolve_across_channels=False, num_output_channels=None,
 kernel_is_vector=False):
-    with tf.name_scope(scope_name)
+    with tf.name_scope(scope_name):
         # input_kernel = [kernel_height, kernel_width, input_tensor:vec_dim]
         # no. feat. maps of convolution = output_kernel_vec_dim
         # Hence: kernel for convolution = [kernel_height, kernel_width, input_tensor:vec_dim output_kernel_vec_dim]
@@ -100,7 +115,7 @@ kernel_is_vector=False):
         #reshape to = [batch, vec_dim, height, width, num_channel]
         input_tensor = tf.transpose(input_tensor, [0,1,3,4,2])
 
-        if(convolve_across_channels===True):
+        if(convolve_across_channels==True):
             _no_input_channels_for_conv = input_tensor_shape[2]
             output_ch = num_output_channels
             if(output_ch==None):
@@ -114,7 +129,7 @@ kernel_is_vector=False):
         output_tensor_shape[2] = output_ch;
         output_tensor = tf.zeros(shape=output_tensor_shape, name="ConvCapInit")
         for o_chan_num in range(output_ch):
-            if(convolve_across_channels===True):
+            if(convolve_across_channels==True):
                 channel_tensor = input_tensor
             else:
                 channel_tensor = input_tensor[:,:,:,:,o_chan_num]
@@ -132,7 +147,7 @@ kernel_is_vector=False):
                     tmp_kernel_tiling[0] = input_tensor_shape[1]
                     kernel = tf.tile(kernel, tmp_kernel_tiling)
 
-            padding_size = np.array()[kernel_height, kernel_width])
+            padding_size = np.array([kernel_height, kernel_width])
             padding_size = (padding_size-1)/2
             padding = np.ones([len(input_tensor_shape), 2])
             padding[2] = np.array([padding_size, padding_size])
