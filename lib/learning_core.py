1c8c5cab0408a63b15e73b2a728e62adcaddf0ad
Krishna Seegoolam <kgs13@ying.doc.ic.ac.uk>
2018-05-11 12:05:48 +0100

    In progress. Eval Classification not working still.

--- lib/learning_core.py
+++ lib/learning_core.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from losses import margin_loss
+import numpy as np
+
 
 class learning_core(object):
   def __init__(self):
@@ -45,37 +48,92 @@ class learning_core(object):
 
 
 
-  def evaluate_classification(model_output, gt_output, this_tower_scope, num_targets=1, loss_type='margin'):
+  def evaluate_classification(self,model_output, gt_output, this_tower_scope, num_targets=1, loss_type='margin'):
     # [] - would be good to rewrite this in the future [unfinished]
     # [] to delete num_targets? - num targets is the number of classifications made per label
+    print(">>>> Find Margin Loss")
     with tf.name_scope('loss'):
         if loss_type == 'margin':
-          classification_loss = _margin_loss(gt_output, model_output)
+          classification_loss = margin_loss(gt_output, model_output)
         else:
           raise NotImplementedError('Not implemented')
 
+    print(">>>> Find Mean Loss")
     with tf.name_scope('total'):
-      batch_classification_loss = tf.reduce_mean(classification_loss, axis=0)
+      print(">>>>>> Batch Loss")
+      print(classification_loss.get_shape().as_list())
+      batch_classification_loss = tf.reduce_mean(classification_loss)
+      print(">>>>>> Add to collection")
       tf.add_to_collection('losses', batch_classification_loss)
-      tf.summary.scalar('batch_classification_cost', batch_classification_loss)
+      print(">>>>>> Creating summary")
+      batch_classification_loss=tf.constant(batch_classification_loss)
+      print(batch_classification_loss.get_shape().as_list())
+      tmp=tf.contrib.summary.scalar(name='batch_classification_cost', tensor=batch_classification_loss)
 
-    all_losses = tf.get_collection('losses', this_tower_scope) # list of tensors returned
+    print(">>>> Add result to collection of loss results for this tower")
+    all_losses = tf.get_collection('losses') # [] , this_tower_scope) # list of tensors returned
     total_loss = tf.add_n(all_losses, name='total_loss') # element-wise addition of the list of tensors
-    tf.summary.scalar('total_loss', total_loss)
+    #print(total_loss.get_shape().as_list())
+    tf.contrib.summary.scalar('total_loss', total_loss)
 
+    print(">>>> Find Accuracy")
+    #%model_output = tf.cast(model_output, np.float32)
+    #gt_output = tf.cast(gt_output, np.float32)
+    print(np.shape(gt_output))
+    print(model_output.get_shape().as_list())
     with tf.name_scope('accuracy'):
         with tf.name_scope('correct_prediction'):
-            _, targets = tf.nn.top_k(gt_output, k=num_targets)
-            _, predictions = tf.nn.top_k(model_output, k=num_targets)
-            missed_targets = tf.sets.set_difference(targets, predictions) # i.e. return only the values which are different (not the same as minus!)
-            num_missed_targets = tf.sets.set_size(missed_targets)
+            print(">>>>> Find GT classifications")
+            #tmp, targets = tf.nn.top_k(gt_output, num_targets)
+            #tmp, targets = tf.nn.top_k(gt_output, num_targets)
+            #targets = tf.argmax(gt_output, axis=1)
+            targets = tf.argmax(gt_output, axis=1)
+            print(">>>>> Find Model classifications")
+            #tmp, predictions = tf.nn.top_k(model_output, num_targets)
+            predictions = tf.argmax(model_output, axis=1)
+            print(">>>>> Find Difference in classifications")
+            #targets = tf.transpose(targets, [1,0])
+            #predictions = tf.transpose(predictions, [1,0])
+            targets = tf.expand_dims(targets, 1)
+            predictions = tf.expand_dims(predictions, 1)
+            print(model_output.dtype)
+            print(gt_output.dtype)
+            #targets = tf.cast(targets, tf.int8)
+            #predictions = tf.cast(predictions, tf.int8)
+            print(targets.dtype)
+            print(predictions.dtype)
+            print(targets.get_shape().as_list())
+            print(predictions.get_shape().as_list())
+            #tmp=tf.subtract(gt_output, model_output)
+            print(">>>>>>> Set Difference")
+            #missed_targets = tf.sets.set_difference(gt_output, model_output) # i.e. return only the values which are different (not the same as minus!)
+            #missed_targets = tf.contrib.metrics.set_difference([[1],[1],[1],[4],[5]],[[2],[1],[1],[3],[5]]) # i.e. return only the values which are different (not the same as minus!)
+            #missed_targets = tf.contrib.metrics.set_difference(targets, predictions) # i.e. return only the values which are different (not the same as minus!)
+            #gt_output_indx = tf.constant(gt_output_indx)
+            #print(gt_output_indx)
+            #print(np.shape(gt_output_indx))
+            tmp=tf.subtract(targets, predictions)
+            #tmp = tf.subtract([[1],[1],[1],[4],[5]],[[2],[1],[1],[3],[5]])
+            '''tmp = tf.nn.in_top_k(model_output, gt_output_indx, num_targets)'''
+            print(">>>>>>> Set Size")
+            #num_missed_targets = tf.sets.set_size(missed_targets)
+            tmp=tf.abs(tmp)
+            print(".")
+            tmp=tf.greater(tmp, 0)
+            print(".")
+            print(tmp.get_shape().as_list())
+            num_missed_targets = tf.reduce_sum(tmp)
+            print(">>>>>>> Set Equal")
             correct = tf.equal(num_missed_targets, 0)
+            print(">>>>> Find Correct/Incorrect")
             almost_correct = tf.less(num_missed_targets, num_targets)
             correct_sum = tf.reduce_sum(tf.cast(correct, tf.float32))
             almost_correct_sum = tf.reduce_sum(tf.cast(almost_correct, tf.float32))
+    print(">>>> Add results to output")
     with tf.name_scope('accuracy'):
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-        tf.summary.scalar('accuracy', accuracy)
-        tf.summary.scalar('correct_prediction_batch', correct_sum)
-        tf.summary.scalar('almost_correct_batch', almost_correct_sum)
+        tf.contrib.summary.scalar('accuracy', accuracy)
+        tf.contrib.summary.scalar('correct_prediction_batch', correct_sum)
+        tf.contrib.summary.scalar('almost_correct_batch', almost_correct_sum)
+    print(">>>> return results")
     return total_loss, correct_sum, almost_correct_sum
