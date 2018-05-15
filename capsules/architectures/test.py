02df0a3ec851bb4848cefa2af5d6196a4491b4a5
Krishna Seegoolam <kgs13@ying.doc.ic.ac.uk>
2018-05-15 17:32:22 +0100

    Training debugging and fixes

--- capsules/architectures/test.py
+++ capsules/architectures/test.py
@@ -27,19 +27,21 @@ class architecture(object):
         #input images = [batch, height,width]
         print(">>>>> Initial Convolution")
         print(np.shape(input_images))
-        layer1 = init_conv_2d(input_images, 20, "Init", kernel_size=9)
+        layer1 = init_conv_2d(input_images, 24, "Init", kernel_size=9)
         print(layer1.get_shape().as_list())
         print(">>>>> Initial Convolution Capsule")
-        layer2 = convolutional_capsule_layer(layer1, 9,9,'ConvCaps',output_kernel_vec_dim=8,convolve_across_channels=True,    num_output_channels=10, kernel_is_vector=False)
+        layer2 = convolutional_capsule_layer(layer1, 9,9,'ConvCaps',output_kernel_vec_dim=8,    num_output_channels=3, strides=[3,3])
         # layer2b = _routing(layer2, layer_dimensions=[batch, vec_dim, num_channels, height, width], squash_biases, num_routing)
-        print(layer2.get_shape().as_list())
-        print(">>>>> Routing 1")
-        layer2b = patch_based_routing(layer2,"primaryrouting", patch_shape=[1,5,5])
-        print(layer2b.get_shape().as_list())
-        print(">>>>> FC Capsule Layer")
-        layer3 = fc_capsule_layer(layer2b, "Layer3", apply_weights=True,    share_weights_within_channel=False, output_vec_dimension=16)
+        #print(layer2.get_shape().as_list())
+        #print(">>>>> Routing 1")
+        #layer2b = patch_based_routing(layer1,"primaryrouting", patch_shape=[1,5,5])
+        #print(layer2b.get_shape().as_list())
+
+        #print(">>>>> FC Capsule Layer")
+        #layer3 = fc_capsule_layer(layer2, "Layer3", apply_weights=True,    share_weights_within_channel=False, output_vec_dimension=16)
+
         print(">>>>> Routing 2")
-        layer3b = routing(layer3, "digitrouting", output_dimensions=[10,1,1],    bias_channel_sharing=True) # [batch, 16, num_ch, height, width] is equal to  [b,16,10,1,1]
+        layer3b = routing(layer2, "digitrouting", output_dimensions=[10,1,1],    bias_channel_sharing=True) # [batch, 16, num_ch, height, width] is equal to  [b,16,10,1,1]
         #layer3b = _patch_based_routing(layer3, patch_shape=None)  32 channel output
         #layer4 = logistic_fc_neural_network(flatten(layer3b))
         print(">>>>> Output")
