4953c6f17c975e40a613bf6615e085ec93e9d129
Krishna Seegoolam <kgs13@ying.doc.ic.ac.uk>
2018-05-15 17:48:36 +0100

    simply convolution for testing purposes.

--- capsules/architectures/test.py
+++ capsules/architectures/test.py
@@ -29,8 +29,10 @@ class architecture(object):
         print(np.shape(input_images))
         layer1 = init_conv_2d(input_images, 24, "Init", kernel_size=9)
         print(layer1.get_shape().as_list())
-        print(">>>>> Initial Convolution Capsule")
-        layer2 = convolutional_capsule_layer(layer1, 9,9,'ConvCaps',output_kernel_vec_dim=8,    num_output_channels=3, strides=[3,3])
+
+        #print(">>>>> Initial Convolution Capsule")
+        #layer2 = convolutional_capsule_layer(layer1, 9,9,'ConvCaps',output_kernel_vec_dim=8,    num_output_channels=3, strides=[3,3])
+
         # layer2b = _routing(layer2, layer_dimensions=[batch, vec_dim, num_channels, height, width], squash_biases, num_routing)
         #print(layer2.get_shape().as_list())
         #print(">>>>> Routing 1")
@@ -41,7 +43,7 @@ class architecture(object):
         #layer3 = fc_capsule_layer(layer2, "Layer3", apply_weights=True,    share_weights_within_channel=False, output_vec_dimension=16)
 
         print(">>>>> Routing 2")
-        layer3b = routing(layer2, "digitrouting", output_dimensions=[10,1,1],    bias_channel_sharing=True) # [batch, 16, num_ch, height, width] is equal to  [b,16,10,1,1]
+        layer3b = routing(layer1, "digitrouting", output_dimensions=[10,1,1],    bias_channel_sharing=True) # [batch, 16, num_ch, height, width] is equal to  [b,16,10,1,1]
         #layer3b = _patch_based_routing(layer3, patch_shape=None)  32 channel output
         #layer4 = logistic_fc_neural_network(flatten(layer3b))
         print(">>>>> Output")
