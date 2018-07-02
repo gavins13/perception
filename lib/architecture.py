01e484f15a30ae66105624c03a0e233b3269c49c
unknown <KGS13@libws-sktr122.ic.ac.uk>
2018-07-02 17:58:50 +0100

    Added abstract class for future architectures/models

---
+++ lib/architecture.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class architecture(ABC):
+    def __init__(self):
+        self.hparams = tf.contrib.training.HParams(
+          decay_rate=0.9,
+          decay_steps=1000.,
+          learning_rate=1.e-5, # 0.001
+          maximum_learning_rate = 1.e-7, # 1.e-7
+        )
+        
+    @abstractmethod
+    def build(self, input_images):
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def loss_func(self, input_images, ground_truth, validation_input_images, validation_ground_truth):
+        raise NotImplementedError()
