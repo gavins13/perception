73b9917099dc63e28262adb6fcd9874518945e13
gavin <gavinseegoolam@outlook.com>
2018-07-03 16:22:44 +0100

    [minor]

--- lib/architecture.py
+++ lib/architecture.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-class architecture(ABC):
+class architecture_base(ABC):
     def __init__(self):
         self.hparams = tf.contrib.training.HParams(
           decay_rate=0.9,
@@ -8,11 +8,11 @@ class architecture(ABC):
           learning_rate=1.e-5, # 0.001
           maximum_learning_rate = 1.e-7, # 1.e-7
         )
-        
+
     @abstractmethod
     def build(self, input_images):
         raise NotImplementedError()
-    
+
     @abstractmethod
     def loss_func(self, input_images, ground_truth, validation_input_images, validation_ground_truth):
         raise NotImplementedError()
