0f09580b00bd846ce84cff7dfa6dddf1ef86e017
Krishna Seegoolam <kgs13@ying.doc.ic.ac.uk>
2019-01-29 15:45:38 +0000

    options for run script

--- run.sh
+++ run.sh
@@ -8,11 +8,48 @@ echo $LD_LIBRARY_PATH
 export TF_CPP_MIN_LOG_LEVEL=0
 export TF_CPP_MIN_VLOG_LEVEL=0 # usually = 1 is okay for debugging
 #python3 -m memory_profiler experiment.py
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+
+for i in "$@"
+do
+case $i in
+    -g=*|--gpu=*)
+    export CUDA_VISIBLE_DEVICES="${i#*=}"
+    shift # past argument=value
+    ;;
+    -n=*|--name=*)
+    EXP_NAME="${i#*=}"
+    shift # past argument=value
+    ;;
+    -d=*|--desc=*)
+    EXP_DESC="${i#*=}"
+    shift # past argument=value
+    ;;
+    --no-email)
+    NO_EMAIL=YES
+    shift # past argument with no value
+    ;;
+    # --default)
+    #DEFAULT=YES
+    #shift # past argument with no value
+    #;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+if [ "$CUDA_VISIBLE_DEVICES" = '' ]; then
+  export CUDA_VISIBLE_DEVICES=3
+fi
+echo "GPU BEING USED  = ${CUDA_VISIBLE_DEVICES}"
 if [ "$1" = 'memory' ]; then
   echo "memory testing"
   python3 -m memory_profiler experiment.py
 else
   echo "no memory testing"
   python3 experiment.py
+fi
+if [ "$NO_EMAIL" = '' ]; then
+  curl -s "https://www.doc.ic.ac.uk/~kgs13/sendemail.php?name="$EXP_NAME"&desc="$EXP_DESC
+  echo "Email Sent."
 fi
