ee8097c87e1bb5823c773ab37c1bc3f630e28d68
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:25:21 +0100

    easy execution

--- experiment.py
+++ experiment.py
@@ -1,15 +1,17 @@
-from lib.multi_gpu_frame import mutli_gpu_model as resources_model
-from capsules.architectures import test as test_caps_architecture
-from datetime import datetime
-from lib.data import Data
+from lib.multi_gpu_frame import multi_gpu_model as resources_model
+from capsules.architectures.test import architecture as test_caps_architecture
+from lib.data_frame import Data
 
-from data.load_mnist import load
+from data.load_mnist import load_data
 from lib.execution import execution
+
+import tensorflow as tf
 
 import os, sys
 ''' Config here '''
 project_path = '/vol/biomedic/users/kgs13/PhD/capsule_networks/first_model'
-
+cpu_only = True
+num_gpus=1
 '''
 System is split into 3 objects:
  - An object which creates the capsule architecture and manages special operations.
@@ -20,19 +22,22 @@ NB/ in the future, the 'learning_core.py' might need to be rethought (as well as
 '''
 
 try:
-
-  DataModel = Data(load, num_gpus=1)
-
-  System = resources_model()
+  tf.enable_eager_execution()
+  DataModel = Data(load_data, num_gpus=num_gpus)
+  print("Start resource manager...")
+  System = resources_model(cpu_only=cpu_only)
+  print("Create Network Architecture...")
   CapsuleNetwork = test_caps_architecture()
+  print("Strap Architecture to Resource Manager")
   System.strap_architecture(CapsuleNetwork)
 
-  with execution(project_path, System, DataModel) as Executer:
+  print("Strap Managed Architecture to a training scheme `Executer`")
+  with execution(project_path, System, DataModel, experiment_name="test") as Executer:
       Executer.run_task(max_steps=1000, save_step=0)
 
 
 
 
 except Exception as e:
-  err_message = e.args[0]
+  err_message = e.args
   print(err_message)
