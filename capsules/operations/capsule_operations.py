1fa5e03e73a3a01e4671a0c169cfce05b00a829a
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:29:15 +0100

    [feat] capsules without squash biases

--- capsules/operations/capsule_operations.py
+++ capsules/operations/capsule_operations.py
@@ -445,7 +445,7 @@ kernel_is_vector=False, upsampling_factor=None, type="SAME", num_routing=3):
 
 
 
-def convolutional_capsule_layer_v2(input_tensor, kernel_height, kernel_width, scope_name,output_kernel_vec_dim=8, strides=[1, 1], num_output_channels=None, type="SAME", num_routing=3, split_routing=False, use_matrix_bias=True):
+def convolutional_capsule_layer_v2(input_tensor, kernel_height, kernel_width, scope_name,output_kernel_vec_dim=8, strides=[1, 1], num_output_channels=None, type="SAME", num_routing=3, split_routing=False, use_matrix_bias=True, use_squash_bias=True):
     print(">>>> %s START" % scope_name)
     with tf.name_scope(scope_name):
         '''if(type=="SAME"):
@@ -574,8 +574,11 @@ def convolutional_capsule_layer_v2(input_tensor, kernel_height, kernel_width, sc
         '''squash_bias_shape = prerouted_output_shape2[1:3] + [1, 1] # [ v_d^l+1, |T^l+1|, 1, 1]'''
         squash_bias_shape = prerouted_output_shape2[1:3] + [prerouted_output_shape2[3], 1] # [ v_d^l+1, |T^l+1|, x'*y', 1]
         print(squash_bias_shape)
-        with tf.variable_scope(scope_name):
-            squash_biases = variables.bias_variable(squash_bias_shape)
+        if(use_squash_bias==True):
+            with tf.variable_scope(scope_name):
+                squash_biases = variables.bias_variable(squash_bias_shape)
+        else:
+                squash_biases = tf.fill(squash_bias_shape, 0.)
         print([1, 1, prerouted_output_shape2[3], 1])
         '''squash_biases = tf.tile(squash_biases, [1, 1, prerouted_output_shape2[3], 1])'''
         # [v_d^l+1, |T^l+1|, x'*y', 1]
