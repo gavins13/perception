142f725c59ed1f456f3b3824c079bc81c80b0c7e
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:32:25 +0100

    [minor] Data Loader is now a callable object

--- experiment.py
+++ experiment.py
@@ -6,20 +6,21 @@ import collections
 from pprint import pprint
 #from data.load_mnist import load_data_light as load_data
 sys.path.insert(0, '/homes/kgs13/biomedic/PhD/sunnybrook-data-dicom/')
-from load_data import load_data
+from load_data import load_data as Data_Loader
 from lib.execution import execution
 DataConfiguration = collections.namedtuple("DataConfiguration", ['project_path', 'execution_type', 'model_load_dir'])
 SystemConfiguration = collections.namedtuple("SystemConfiguration", ["cpu_only", "num_gpus", "eager", "mini_batch_size", "test_architecture", "validation_size"])
 
 ''' Config here '''
 from capsules.architectures.v0_rev0_1 import architecture as Architecture
-from capsules.architectures.v1_rev1_3 import architecture as TestArchitecture
-experiment_name = 'v1_rev1_3'
+from capsules.architectures.v1_rev4_2 import architecture as TestArchitecture
+experiment_name = 'v1_rev4_2'
 data_config = DataConfiguration(project_path='/vol/biomedic/users/kgs13/PhD/capsule_networks/first_model',execution_type='train',
-    model_load_dir='v1_rev1_3_2018-07-04-16:00:27.537862')
-system_config = SystemConfiguration(cpu_only=False, num_gpus=1, eager=False, mini_batch_size=4, test_architecture=True, validation_size=4)
+    model_load_dir='v1_rev4_2_2018-07-19-21:02:49.300450')
+system_config = SystemConfiguration(cpu_only=False, num_gpus=1, eager=False, mini_batch_size=2, test_architecture=True, validation_size=2)
+load_data = Data_Loader(dataset="acc=0.29")
 ''' End Config '''
-
+# v1_rev1_3_2018-07-04-16:00:27.537862(basereconcaps)  v1_rev1_2_2018-07-04-12:11:47.413890(deepreconcaps) baseline_v1_rev1_3_2018-07-10-12:39:28.799803(baseline for reconcaps) v1_rev3_1_2018-07-04-21:32:34.241743(iterativecaps) v1_rev4_1_2018-07-13-17:22:41.612039(3x3 convcps) v1_rev4_2_2018-07-17-15:23:33.786890(5x5 convcaps mb=2)     baseline_v1_rev1_3_nocaps_2018-07-17-17:46:53.833824(basereconcaps without caps - attemp2: v1_rev4_2_2018-07-17-20:16:35.964292)   SR_v1_rev4_1_2018-07-18-14:04:32.664220(3x3 concaps) 'v1_rev4_2_2018-07-19-10:47:41.929848'
 try:
   if system_config.eager==True:
     tf.enable_eager_execution()
