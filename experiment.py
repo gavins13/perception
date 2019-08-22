a041c0e7e5730e5e3e3fda86b42839fdddd36d32
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:27:03 +0100

    tidy code

--- experiment.py
+++ experiment.py
@@ -1,60 +1,31 @@
 from lib.multi_gpu_frame import multi_gpu_model as resources_model
-from capsules.architectures.v0_rev0_1 import architecture as architecture
-from capsules.architectures.v0_rev1_2_test import architecture as test_architecture
 from lib.data_frame import Data
-
 import os, sys
 import tensorflow as tf
-
-
+import collections
 #from data.load_mnist import load_data_light as load_data
 sys.path.insert(0, '/homes/kgs13/biomedic/PhD/sunnybrook-data-dicom/')
 from load_data import load_data
-
 from lib.execution import execution
+DataConfiguration = collections.namedtuple("DataConfiguration", ['project_path', 'execution_type'])
+SystemConfiguration = collections.namedtuple("SystemConfiguration", ["cpu_only", "num_gpus", "eager", "mini_batch_size"])
 
 ''' Config here '''
-project_path = '/vol/biomedic/users/kgs13/PhD/capsule_networks/first_model'
-cpu_only = False
-num_gpus=1
-eager=False
-test=True
-mini_batch_size=4
-
-''' Logging '''
-import logging
-
-# get TF logger
-log = logging.getLogger('tensorflow')
-log.setLevel(logging.DEBUG)
-
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# create file handler which logs even debug messages
-fh = logging.FileHandler(project_path + '/logs/' + 'tensorflow.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-log.addHandler(fh)
-
-'''
-System is split into 3 objects:
- - An object which creates the capsule architecture and manages special operations.
- - An object which manages the system resources (e.g. GPUs) and divides the workload across available resources (GPUs), at the ending producing a suitable summary from all resources
- - An object which manages the training of the system, number of iterations run, and saving the model to the create points
-
-NB/ in the future, the 'learning_core.py' might need to be rethought (as well as multi_gpu_frame.py) as it only really works for classification-type problems
-'''
+from capsules.architectures.v0_rev0_1 import architecture as architecture
+from capsules.architectures.v0_rev1_2_test import architecture as test_architecture
+data_config = DataConfiguration('/vol/biomedic/users/kgs13/PhD/capsule_networks/first_model','train')
+system_config = SystemConfiguration(False, num_gpus=1, False, mini_batch_siz=4)
+''' End Config '''
 
 try:
-  if eager==True:
+  if data_config.eager==True:
     tf.enable_eager_execution()
-  DataModel = Data(load_data, num_gpus=num_gpus)
+  DataModel = Data(load_data, num_gpus=system_config.num_gpus)
   print("Start resource manager...")
-  System = resources_model(cpu_only=cpu_only,eager=eager)
+  System = resources_model(cpu_only=system.config.cpu_only,eager=system.config.eager)
   print("Create Network Architecture...")
   if(test==True):
-      print("Runnin Test Architecture... *** MAIN ARCHITECTURE NOT BEING USED ***")
+      print("Running Test Architecture... *** MAIN ARCHITECTURE NOT BEING USED ***")
       CapsuleNetwork = test_architecture()
   else:
       CapsuleNetwork = architecture()
@@ -62,15 +33,8 @@ try:
   System.strap_architecture(CapsuleNetwork)
 
   print("Strap Managed Architecture to a training scheme `Executer`")
-  #with execution(project_path, System, DataModel, experiment_name="test") as Executer:
-  #    Executer.run_task(max_steps=1000, save_step=0)
-  Executer = execution(project_path, System, DataModel, experiment_name="test", max_steps_to_save=1000, mini_batch_size=mini_batch_size)
-  Executer.__enter__()
-  Executer.run_task(max_steps=1000, save_step=1)
-  Executer.__exit__(None,None,None)
-
-
-
+  with execution(project_path, System, DataModel, experiment_name="test", max_steps_to_save=1000, mini_batch_size=mini_batch_size, type=execution_type) as Executer:
+      Executer.run_task(max_steps=1000, save_step=1)
 except Exception as e:
   err_message = e.args
   print(err_message)
