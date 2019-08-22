b10458663e2300c94cd673c38f2f8f867fef0e46
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:26:05 +0100

    Memory Profiler

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ export CPATH=$CPATH:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$folder2
 echo $LD_LIBRARY_PATH
 export TF_CPP_MIN_LOG_LEVEL=0
 export TF_CPP_MIN_VLOG_LEVEL=0 # usually = 1 is okay for debugging
+#python3 -m memory_profiler experiment.py
 python3 experiment.py
