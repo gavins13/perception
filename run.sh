543ed3c210f0d07b2499c87a9f87fa818b68efdd
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:26:13 +0100

    memory profiling

--- run.sh
+++ run.sh
@@ -8,4 +8,10 @@ echo $LD_LIBRARY_PATH
 export TF_CPP_MIN_LOG_LEVEL=0
 export TF_CPP_MIN_VLOG_LEVEL=0 # usually = 1 is okay for debugging
 #python3 -m memory_profiler experiment.py
-python3 experiment.py
+if [ "$1" = 'memory' ]; then
+  echo "memory testing"
+  python3 -m memory_profiler experiment.py
+else
+  echo "no memory testing"
+  python3 experiment.py
+fi
