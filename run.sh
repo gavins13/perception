8cae6201ff6046c1b15b0ca423ed164ff1f62843
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:26:45 +0100

    debug off

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$fol
 export CPATH=$CPATH:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$folder2:/vol/cuda/$folder2/lib64
 echo $LD_LIBRARY_PATH
 export TF_CPP_MIN_LOG_LEVEL=0
-export TF_CPP_MIN_VLOG_LEVEL=1 # usually = 1 is okay for debugging
+export TF_CPP_MIN_VLOG_LEVEL=0 # usually = 1 is okay for debugging
 #python3 -m memory_profiler experiment.py
 if [ "$1" = 'memory' ]; then
   echo "memory testing"
