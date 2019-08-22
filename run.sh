81677e3a75e6294d53be4cbf61694602e8a03741
Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-08-22 13:25:37 +0100

    Run with cuda and cudnn for tensorflow-gpu

---
+++ run.sh
@@ -0,0 +1,8 @@
+folder=9.0.176
+folder2=8.0.61-cudnn.7.0.2
+export PATH=$PATH:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$folder2:/vol/cuda/$folder2/lib64
+export LIBRARY_PATH=$LIBRARY_PATH:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$folder2:/vol/cuda/$folder2/lib64
+export LD_LIBRARY_PATH=:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$folder2:/vol/cuda/$folder2/lib64
+export CPATH=$CPATH:/vol/cuda/$folder:/vol/cuda/$folder/lib64:/vol/cuda/$folder2:/vol/cuda/$folder2/lib64
+echo $LD_LIBRARY_PATH
+python3 experiment.py
