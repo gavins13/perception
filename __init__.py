0da35a4e2780f62a3e58cc2b36613d6954e15845
Gavin Seegoolam <22893238+gavinlive@users.noreply.github.com>
2020-01-29 12:08:21 +0000

    packaging

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 from .main import Experiment
+from .lib.model import Model
+from .lib.dataset import Dataset
+from .lib.misc import printt
+from . import lib
+from . import data
+from . import models
