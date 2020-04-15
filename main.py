1d2f16b022d3f6df8b000386fb26dcae1bcfcad8
gavin <22893238+gavinlive@users.noreply.github.com>
2020-04-15 19:01:50 +0100

    Tidy up, added more options for metrics

--- main.py
+++ main.py
@@ -1,54 +1,61 @@
 #!python
 import os
-if __name__ == "__main__":
-    from lib.misc import printt
-    print("running now...")
-    import matplotlib
-    matplotlib.use("agg") #qt5agg
-    import json
-    if os.path.isfile("Config.perception") is True:
-        with open("Config.perception", "r") as config_file:
-            Config = json.load(config_file)
-    else:
-        printt("Config.perception doesn't exist", error=True, stop=True)
-    experiment_id = Config['defaults']['experiment_id']
-    experiment_type = Config['defaults']['experiment_type']
-    from lib.execution import Execution
-    from lib.dataset import Dataset
-    from lib.experiments import Experiments as ExperimentsManager
-    from lib.misc import detect_cmd_arg, path as prepare_path
-else:
-    from .lib.misc import printt
-    from .lib.execution import Execution
-    from .lib.dataset import Dataset
-    from .lib.experiments import Experiments as ExperimentsManager
-    from .lib.misc import detect_cmd_arg, path as prepare_path
-
+from .lib.misc import printt
+from .lib.execution import Execution
+from .lib.dataset import Dataset
+from .lib.experiments import Experiments as ExperimentsManager
+from .lib.misc import detect_cmd_arg, path as prepare_path
 import sys
 import importlib
+import tensorflow as tf
 
 
 def Experiment(experiment_id, experiment_type='test', execute=False, gpu=None,
-  tensorboard_only=False, reset=False, experiments_file=None, debug_level=None):
+  tensorboard_only=False, reset=False, experiments_file=None, debug_level=None,
+  gradient_taping=False, debug=False, command_line_arguments_enabled=True,
+  metrics_enabled=False, metrics_printing_enabled=False):
     '''
     experiment_id: (str) experiment_id from the JSON files
-    experiment_type: (str) 'train' or 'test'
-    (optional) execute: (bool) Execute before returning?
+    experiment_type: (str) 'train' or 'test'. Default: 'test'
+    (optional) execute: (bool) Execute before returning?. Default: False
     (optional) gpu: (int) Which GPU number to use
-    (optional) tensorboard_only: (bool) Only start TensorBoard (TB)? Default is to start TB with training
-    (optional) reset: (bool) train from scratch?
+    (optional) tensorboard_only: (bool) Only start TensorBoard (TB)? Default: False (i.e. start TB if training, otherwise do not start)
+    (optional) reset: (bool) train from scratch? Default: False
+    (optional) gradient_taping: (bool) Using GradientTape or train with Keras API? Use GradientTape (True) for easier debugging. Default: False
+    (optional) debug: (bool) If True, then loss_func doesn't use the tf.function wrapper (for graph building) and if gradient_taping is False (i.e. using Keras API) then it forces the Keras model compiler to run_eagerly. Default: False.
+    (optional) command_line_arguments_enabled: (bool) Allow options to specified from command arguments. Default: True
+
+    Other command line arguments:
+    --debug-level: (int) From 0 to 5. 0: Print nothing. 1: Print errors. 2: Print warnings. 3: Print Information. 4: Print Debug Information 5: Print Everything
+
+    NOTE: the command line argument 'debug_level' is not affected by the command_line_arguments_enabled option
+
+    NOTE: for full debugging mode, gradient_taping should be True, and debug should be True. Future versions of Perception will probably automatically switch gradient_taping to True when debug is True.
     '''
 
+    if command_line_arguments_enabled is True:
+        experiment_id = detect_cmd_arg("experiment_id", false_val=experiment_id)
+        experiment_id = detect_cmd_arg("experiment", false_val=experiment_id)
+        experiments_file = detect_cmd_arg("experiments_file", false_val=experiments_file)
+        experiment_type = detect_cmd_arg("type", retrieve_val=True, false_val=experiment_type, val_dtype=str)
+        gpu = detect_cmd_arg("gpu", false_val=gpu, val_dtype=int)
+        tensorboard_only = detect_cmd_arg("tensorboard", retrieve_val=False, false_val=tensorboard_only)
+        tensorboard_only_2 = detect_cmd_arg("tensorboard_only", retrieve_val=False, false_val=tensorboard_only)
+        tensorboard_only = (tensorboard_only or tensorboard_only_2)
+        reset = detect_cmd_arg("reset", retrieve_val=False, false_val=reset)
+        gradient_taping = detect_cmd_arg("gradient_taping", retrieve_val=False, false_val=gradient_taping)
+        debug = detect_cmd_arg("debug", retrieve_val=False, false_val=debug)
+        metrics_enabled = detect_cmd_arg("metrics_enabled", retrieve_val=False, false_val=metrics_enabled)
+        metrics_enabled = detect_cmd_arg("metrics", retrieve_val=False, false_val=metrics_enabled)
+        metrics_printing_enabled = detect_cmd_arg("metrics_printing_enabled", retrieve_val=False, false_val=metrics_printing_enabled)
+        metrics_printing_enabled = detect_cmd_arg("metrics_printing", retrieve_val=False, false_val=metrics_printing_enabled)
 
-    gpu = detect_cmd_arg("gpu", false_val=gpu, val_dtype=int)
-    tensorboard_only = detect_cmd_arg("tensorboard", retrieve_val=False, false_val=tensorboard_only)
-    tensorboard_only_2 = detect_cmd_arg("tensorboard_only", retrieve_val=False, false_val=tensorboard_only)
-    tensorboard_only = (tensorboard_only or tensorboard_only_2)
-    reset = detect_cmd_arg("reset", retrieve_val=False, false_val=reset)
+
 
     experiments = ExperimentsManager(experiments_file=experiments_file)
     if tensorboard_only is True:
         gpu = None
+        reset = False
         printt("Only Tensorboard starting", info=True)
 
     if debug_level is not None:
@@ -66,7 +73,13 @@ def Experiment(experiment_id, experiment_type='test', execute=False, gpu=None,
         os.environ["CUDA_VISIBLE_DEVICES"] = ''
         printt("GPU not being used", warning=True)
 
-    if not(experiment_type in ['train', 'test', 'evaluate']):
+    memory_growth = detect_cmd_arg("memory_growth", retrieve_val=False, false_val=False)
+    if memory_growth is True:
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+    if not(experiment_type in ['train', 'test', 'testing', 'evaluate']):
         printt('Execution mode invalid', error=True, stop=True)
 
     if not(experiment_id in experiments.keys()):
@@ -87,7 +100,7 @@ def Experiment(experiment_id, experiment_type='test', execute=False, gpu=None,
     else:
         printt("Architecture/Model not specified", error=True, stop=True)
 
-    
+
     if 'module_args' in experiments[experiment_id].keys():
         module_args = experiments[experiment_id]['module_args']
     else:
@@ -121,11 +134,19 @@ def Experiment(experiment_id, experiment_type='test', execute=False, gpu=None,
     Dataset = Dataset_Frame()
     # Dataset_Frame.create(); Dataset = Dataset_Frame
 
+    '''
+    Experiment save path (relative to the perception save path or `save_path')
+    '''
     if 'save_folder_name' in experiments[experiment_id].keys():
         save_folder_name = experiments[experiment_id]["save_folder_name"]
+        printt("Save folder path: {}".format(save_folder_name), info=True)
     else:
         save_folder_name = None
+        printt("No save folder. Creating one for you...", info=True)
 
+    '''
+    Perception save path
+    '''
     if 'save_path' in experiments[experiment_id].keys():
         save_path = experiments[experiment_id]["save_path"]
     else:
@@ -138,28 +159,6 @@ def Experiment(experiment_id, experiment_type='test', execute=False, gpu=None,
         experiment_type=experiment_type, execute=execute,
         tensorboard_only=tensorboard_only, experiment_id=experiment_id,
         reset=reset, perception_save_path=save_path,
-        experiments_manager=experiments)
-
-
-
-
-
-if __name__ == "__main__":
-    experiment_id = detect_cmd_arg("experiment_id", false_val=experiment_id)
-    experiment_id = detect_cmd_arg("experiment", false_val=experiment_id)
-    experiment_filename = detect_cmd_arg("experiments_file", false_val=None)
-    experiment_type = detect_cmd_arg("type", false_val=experiment_type)
-    gpu = detect_cmd_arg("gpu", false_val=None, val_dtype=int)
-    tensorboard_only = detect_cmd_arg("tensorboard", retrieve_val=False)
-    tensorboard_only_2 = detect_cmd_arg("tensorboard_only", retrieve_val=False)
-    tensorboard_only = (tensorboard_only or tensorboard_only_2)
-    reset = detect_cmd_arg("reset", retrieve_val=False)
-    ThisExperiment = Experiment(
-                        experiment_id,
-                        experiment_type,
-                        experiments_file=experiment_filename,
-                        execute=True,
-                        gpu=gpu,
-                        tensorboard_only=tensorboard_only,
-                        reset=reset
-                    )
+        experiments_manager=experiments, gradient_taping=gradient_taping,
+        debug=debug, metrics_enabled=metrics_enabled,
+        metrics_printing_enabled=metrics_printing_enabled)
