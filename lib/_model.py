f9db3620a35e94903ee2eba1855fd013d51569d4
gavin <22893238+gavinlive@users.noreply.github.com>
2020-05-16 12:01:33 +0100

    Tf2.2 compatibility fixed

--- lib/_model.py
+++ lib/_model.py
@@ -282,8 +282,8 @@ class __Model__(object):
                 return {}
             for i, optimizer_models in enumerate(self.__optimisers_models__):
                 if tf.__version__ == '2.2.0' or tf.__version__ == '2.1.0' or tf.__version__ == '2.2.0-dev20200301':
+                    original_data = data
                     if tf.__version__ == '2.1.0':
-                        original_data = data
                         data,data_y_None,data_sampleweights_None = optimizer_models["models"][0]._standardize_user_data(
                             data, None, sample_weight=None, class_weight=None,
                             extract_tensors_from_dataset=True)
@@ -291,7 +291,8 @@ class __Model__(object):
                         data = data + list(data_y_None or []) + list(data_sampleweights_None or [])
                         if not isinstance(K.symbolic_learning_phase(), int):
                             data += [True]  # Add learning phase value.
-
+                    elif tf.__version__ == '2.2.0':
+                        data = tf.compat.v1.data.make_one_shot_iterator(tf.data.Dataset.from_tensors(data))
                     vals = self.__TEMP__trainfunctions[i](data)
                     data = original_data
                 else:
