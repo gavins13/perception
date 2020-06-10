78d8af6ceaf7ec2dd6e8b7b5d2c16ced27ec9218
Gavin S <22893238+gavinlive@users.noreply.github.com>
2020-06-10 11:05:26 +0100

    minor

--- data/jose/__init__.py
+++ data/jose/__init__.py
@@ -3,7 +3,7 @@ import os, sys
 
 perception_path =os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "../../../")
-import perception_tf2 as perception
+import perception
 
 printt = perception.printt
 DatasetBase = perception.Dataset
