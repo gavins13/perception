359cb2a87638a0f2c4031bb9e0ca99d51bbe2dfd
Gavin Seegoolam <22893238+gavinlive@users.noreply.github.com>
2021-06-25 19:19:55 +0100

    added KerasModel

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
-from .main import Experiment
-from .lib.model import Model
+from .lib.model import Model, KerasModel, KerasModel as TFModel
 from .lib.dataset import Dataset
 from .lib.misc import printt
+from .lib.experiments import Experiments as ExperimentsManager
+from .main import Experiment
+from .prototype import Experiment as Prototype
 from . import lib
 from . import data
 from . import models
